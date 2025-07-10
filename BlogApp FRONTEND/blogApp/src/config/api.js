0771b7fdfb5c9dc555227fb730d949351ca4fa84
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-07-11 01:33:35 +0530

    fixing the base url

--- BlogApp FRONTEND/blogApp/src/config/api.js
+++ BlogApp FRONTEND/blogApp/src/config/api.js
@@ -1,3 +1,5 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
+// const BASE_URL = `https://blogapp-oh9i.onrender.com` || "http://localhost:5000";
+// const BASE_URL = `http://localhost:5000`
 
 export default BASE_URL;
