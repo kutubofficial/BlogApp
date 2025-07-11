9e6afb2233d9fef87ccb43773360c6280d09ab7c
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-07-11 13:13:00 +0530

    updated backend deployed BASE_URL in api.js and .env file

--- Blog App BACKEND/config/db.js
+++ Blog App BACKEND/config/db.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
   const MONGODB_URL=process.env.MONGODB_URL
   const MONGODB_ATLAS=process.env.MONGODB_ATLAS
   try {
+    // const client = await mongoose.connect(MONGODB_URL);
     const client = await mongoose.connect(MONGODB_ATLAS);
     // console.log("Database connected successfully!", client.connection.host);
     console.log("Database connected successfully!");
--- BlogApp FRONTEND/blogApp/src/config/api.js
+++ BlogApp FRONTEND/blogApp/src/config/api.js
@@ -1,5 +1,3 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
-// const BASE_URL = `https://blogapp-oh9i.onrender.com` || "http://localhost:5000";
-// const BASE_URL = `http://localhost:5000`
-
+console.log("BASE_URL:", import.meta.env.VITE_BACKEND_URL);
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 export default BASE_URL;
